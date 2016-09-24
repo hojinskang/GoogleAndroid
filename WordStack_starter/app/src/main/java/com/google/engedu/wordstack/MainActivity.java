8659153032bf3488c3b16958df17b0b38313291e
Hojin Sylvia Kang <hojinskang@gmail.com>
2016-09-24 15:45:42 -0400

    Added third assignment

---
+++ WordStack_starter/app/src/main/java/com/google/engedu/wordstack/MainActivity.java
@@ -0,0 +1,158 @@
+package com.google.engedu.wordstack;
+
+import android.content.res.AssetManager;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int WORD_LENGTH = 5;
+    public static final int LIGHT_BLUE = Color.rgb(176, 200, 255);
+    public static final int LIGHT_GREEN = Color.rgb(200, 255, 200);
+    private ArrayList<String> words = new ArrayList<>();
+    private Random random = new Random();
+    private StackedLayout stackedLayout;
+    private Stack<LetterTile> placedTiles = new Stack<LetterTile>();
+    private String word1, word2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream inputStream = assetManager.open("words.txt");
+            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            while((line = in.readLine()) != null) {
+                String word = line.trim();
+                if (word.length() >= WORD_LENGTH) {
+                    words.add(word);
+                }
+            }
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(this, "Could not load dictionary", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        LinearLayout verticalLayout = (LinearLayout) findViewById(R.id.vertical_layout);
+        stackedLayout = new StackedLayout(this);
+        verticalLayout.addView(stackedLayout, 3);
+
+        View word1LinearLayout = findViewById(R.id.word1);
+        //word1LinearLayout.setOnTouchListener(new TouchListener());
+        word1LinearLayout.setOnDragListener(new DragListener());
+        View word2LinearLayout = findViewById(R.id.word2);
+        //word2LinearLayout.setOnTouchListener(new TouchListener());
+        word2LinearLayout.setOnDragListener(new DragListener());
+    }
+
+    private class TouchListener implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN && !stackedLayout.empty()) {
+                LetterTile tile = (LetterTile) stackedLayout.peek();
+                tile.moveToViewGroup((ViewGroup) v);
+                if (stackedLayout.empty()) {
+                    TextView messageBox = (TextView) findViewById(R.id.message_box);
+                    messageBox.setText(word1 + " " + word2);
+                }
+                placedTiles.push(tile);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private class DragListener implements View.OnDragListener {
+
+        public boolean onDrag(View v, DragEvent event) {
+            int action = event.getAction();
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    v.setBackgroundColor(LIGHT_BLUE);
+                    v.invalidate();
+                    return true;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    v.setBackgroundColor(LIGHT_GREEN);
+                    v.invalidate();
+                    return true;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    v.setBackgroundColor(LIGHT_BLUE);
+                    v.invalidate();
+                    return true;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    v.setBackgroundColor(Color.WHITE);
+                    v.invalidate();
+                    return true;
+                case DragEvent.ACTION_DROP:
+                    // Dropped, reassign Tile to the target Layout
+                    LetterTile tile = (LetterTile) event.getLocalState();
+                    tile.moveToViewGroup((ViewGroup) v);
+                    if (stackedLayout.empty()) {
+                        TextView messageBox = (TextView) findViewById(R.id.message_box);
+                        messageBox.setText(word1 + " " + word2);
+                    }
+                    placedTiles.push(tile);
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    public boolean onStartGame(View view) {
+        LinearLayout word1LinearLayout = (LinearLayout) findViewById(R.id.word1);
+        word1LinearLayout.removeAllViews();
+        LinearLayout word2LinearLayout = (LinearLayout) findViewById(R.id.word1);
+        word2LinearLayout.removeAllViews();
+
+        TextView messageBox = (TextView) findViewById(R.id.message_box);
+        messageBox.setText("Game started");
+        String word1 = words.get(random.nextInt(words.size()));
+        String word2 = words.get(random.nextInt(words.size()));
+        int i = word1.length() - 1, j = word2.length() - 1;
+        while (i >= 0 && j >= 0) {
+            if (random.nextInt(2) == 0) {
+                stackedLayout.push(new LetterTile(this, word1.charAt(i)));
+                i--;
+            } else {
+                stackedLayout.push(new LetterTile(this, word2.charAt(j)));
+                j--;
+            }
+        }
+        while (i >= 0) {
+            stackedLayout.push(new LetterTile(this, word1.charAt(i)));
+            i--;
+        }
+        while (j >= 0) {
+            stackedLayout.push(new LetterTile(this, word2.charAt(j)));
+            j--;
+        }
+        return true;
+    }
+
+    public boolean onUndo(View view) {
+        if (!placedTiles.isEmpty()) {
+            LetterTile tile = placedTiles.pop();
+            tile.moveToViewGroup(stackedLayout);
+            stackedLayout.push(tile);
+        }
+        return true;
+    }
+}
